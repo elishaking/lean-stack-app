8c5eb684280fe48a5c5a4250700ba9bbf942d41c
King Elisha <ek.chibueze@gmail.com>
2020-07-17 21:44:10 +0100

    feat: added <MessageSection />

---
+++ src/components/organisms/message-section/index.tsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+import "../section-style.css";
+import { User } from "../../../interfaces/user";
+import { UserList } from "../user-list";
+import { Message } from "../../../interfaces/message";
+import { MessageList } from "../message-list";
+
+interface TProps {
+  user: User;
+  messages: Message[];
+  addMessage: (message: Message) => void;
+  openMessage: (message: Message) => void;
+}
+
+export class MessageSection extends Component<TProps> {
+  state = {
+    message: "",
+  };
+
+  onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      message: e.target.value,
+    });
+  };
+
+  addMessage = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    this.props.addMessage({
+      id: this.state.message,
+      text: this.state.message,
+      date: Date.now(),
+      user: this.props.user,
+    });
+    this.setState({
+      message: "",
+    });
+  };
+
+  render() {
+    const { message } = this.state;
+    const { messages } = this.props;
+
+    return (
+      <div className="message-section">
+        <MessageList messages={messages} />
+        <form onSubmit={this.addMessage}>
+          <input
+            name="message"
+            placeholder="Add Message"
+            onChange={this.onChange}
+            value={message}
+          />
+        </form>
+      </div>
+    );
+  }
+}
